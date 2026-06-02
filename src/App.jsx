507730ed01e4f172b6147c9da03df4a36a17ebf9
Monu Rajput <monurajput@example.com>
2026-06-02 19:20:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Homepage from './pages/Homepage';
-import Aboutpage from './pages/Aboutpage';
-import Projectpage from './pages/Projectpage';
-import Contactpage from './pages/Contactpage';
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Aboutpage from "./pages/Aboutpage";
+import Projectpage from "./pages/Projectpage";
+import Contactpage from "./pages/Contactpage";
 
-import './App.css'
-import { SiHomepage } from 'react-icons/si';
+import "./App.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path:  "/",
-    element: <Homepage/>
+    path: "/",
+    element: <Homepage />,
   },
   {
-    path:  "/about",
-    element: <Aboutpage/>
+    path: "/about",
+    element: <Aboutpage />,
   },
   {
-    path:  "/project",
-    element: <Projectpage/>
+    path: "/project",
+    element: <Projectpage />,
   },
   {
-    path:  "/contact",
-    element: <Contactpage/>
-  }
-
+    path: "/contact",
+    element: <Contactpage />,
+  },
 ]);
 
-function App(){
-  return <RouterProvider router={router}/>
+function App() {
+  return <RouterProvider router={router} />;
 }
+
 export default App;
