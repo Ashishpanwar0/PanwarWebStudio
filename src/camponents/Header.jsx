fe37acdc343a816bbba2cd01fa902ea8d6960582
Monu Rajput <monurajput@example.com>
2026-06-02 18:40:45 +0530

    update

--- src/camponents/Header.jsx
+++ src/camponents/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Menu, X } from "lucide-react"; // icon library
-import Logo from "../assets/Panwar_Web.png"
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/Panwar_Web.png";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,18 +9,33 @@ const Header = () => {
   return (
     <header className="shadow-md sticky top-0 z-50 bg-white">
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
-        
+
         {/* Logo */}
         <div className="flex items-center gap-2">
-        <img src={Logo} alt="Panwar Web Logo" className="h-12 w-25"/>
+          <img
+            src={Logo}
+            alt="Panwar Web Logo"
+            className="h-12 w-25"
+          />
         </div>
 
         {/* Desktop Menu */}
         <nav className="hidden text-[#3b1f0f] md:flex items-center gap-30 font-medium">
-          <a href="/" className="hover:black hover:underline">Home</a>
-          <a href="/about" className="hover:black hover:underline">About</a>
-          <a href="/project" className="hover:black hover:underline">Projects</a>
-          <a href="/contact" className="hover:black hover:underline">Contact</a>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+
+          <Link to="/about" className="hover:underline">
+            About
+          </Link>
+
+          <Link to="/project" className="hover:underline">
+            Projects
+          </Link>
+
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
         </nav>
 
         {/* Button */}
@@ -40,10 +56,37 @@ const Header = () => {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden px-6 py-4 space-y-4 text-[#3b1f0f]">
-          <a href="#home" className="block hover:black hover:underline">Home</a>
-          <a href="#about" className="block hover:black hover:underline">About</a>
-          <a href="#projects" className="block hover:black hover:underline">Projects</a>
-          <a href="#contact" className="block hover:black hover:underline">Contact</a>
+          <Link
+            to="/"
+            className="block hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/about"
+            className="block hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </Link>
+
+          <Link
+            to="/project"
+            className="block hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
+            Projects
+          </Link>
+
+          <Link
+            to="/contact"
+            className="block hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
+            Contact
+          </Link>
 
           <button className="w-full bg-[#1E0D00] hover:bg-[#8b5e3c] text-white px-5 py-2 rounded-lg font-semibold transition">
             Explore More
